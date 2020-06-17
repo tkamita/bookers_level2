576653c0b57a33ef927000f47ffcf187468b579a
嘉味田崇 <kamitatakashi@kamidatakashinoMacBook-Air.local>
2020-06-18 02:00:59 +0900

    add create action

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,2 +1,15 @@
 class BookCommentsController < ApplicationController
+  def create
+    book = Book.find(params[:book_id])
+    comment = current_user.book_comments.new(book_comment_params)
+    comment.book_id = book.id
+    comment.save
+    redirect_to book_path(book)
+  end
+
+  private
+
+  def book_comment_params
+    params.require(:book_comment).permit(:user_id, :book_id, comment)
+  end
 end
