8213a02d8d09c0b03e6762b852b9d572ad92f8ac
嘉味田崇 <kamitatakashi@kamidatakashinoMacBook-Air.local>
2020-06-17 23:29:13 +0900

    finish favorite

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,17 +1,17 @@
 class FavoritesController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    fovorite = current_user.favorites.new(book_id: book.id)
-    fovorite.save
+    # book = Book.find(params[:book_id])
+    # fovorite = current_user.favorites.new(book_id: book.id)
+    # fovorite.save
     # redirect_to book_path(book)
-    # @favorite = current_user.favorites.create(book_id: params[:book_id])
+    @favorite = current_user.favorites.create(book_id: params[:book_id])
     redirect_back(fallback_location: root_path)
   end
 
 
   def destroy
-    @book = Book.find(params[:book_id]) 
-    @fovorite = current_user.favorites.find_by(book_id: @book.id)
+    @book = Book.find(params[:book_id])
+    @favorite = current_user.favorites.find_by(book_id: @book.id)
     @favorite.destroy
     redirect_back(fallback_location: root_path)
   end
--- app/models/book.rb
+++ app/models/book.rb
@@ -6,7 +6,7 @@ class Book < ApplicationRecord
   	validates :body, presence: true
 		validates :body, length: { maximum: 200 }
 		
-	def favorited_by?(user)
-		favorites.where(user_id: user.id).exists?
-	end
+	# def favorited_by?(user)
+	# 	favorites.where(user_id: user.id).exists?
+	# end
 end
