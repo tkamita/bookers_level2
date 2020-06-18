b5e086ac696daa87cee06b05b481e096fe157df7
嘉味田崇 <kamitatakashi@kamidatakashinoMacBook-Air.local>
2020-06-18 12:59:45 +0900

    add book_comment

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,4 +1,7 @@
 class BookCommentsController < ApplicationController
+  before_action :authenticate_user!
+
+  
   def create
     book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
@@ -7,9 +10,15 @@ class BookCommentsController < ApplicationController
     redirect_to book_path(book)
   end
 
+  def destroy
+    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
+    redirect_to book_path(params[:book_id])
+  end
+
+
   private
 
   def book_comment_params
-    params.require(:book_comment).permit(:user_id, :book_id, comment)
+    params.require(:book_comment).permit(:user_id, :book_id, :comment)
   end
 end
