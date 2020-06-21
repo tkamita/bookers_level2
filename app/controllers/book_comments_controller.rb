a5e27ef7e9e91717ea0fdbf6c16cf43474ed387f
嘉味田崇 <kamitatakashi@kamidatakashinoMacBook-Air.local>
2020-06-21 19:25:00 +0900

    add book_comment ajax

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -3,31 +3,20 @@ class BookCommentsController < ApplicationController
 
   
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
+    comment.book_id = @book.id
     comment.save
-    redirect_to book_path(book)
+    # redirect_to book_path(book)
   end
-  # def create
-  #   @book = Book.find(params[:book_id])
-  #   #投稿に紐づいたコメントを作成
-  #   @book_comment = @book.book_comments.build(book_comment_params)
-  #   @book_comment.user_id = current_user.id
-  #   @book_comment.save
-  #   # render :index
-  # end
+  
 
   def destroy
-    book_comment = BookComment.find(params[:format])
-    book_comment.destroy
-    redirect_back(fallback_location: root_path)
+    @book = Book.find(params[:book_id])
+    @book_comment = BookComment.find(params[:comment])
+    @book_comment.destroy
+    # redirect_back(fallback_location: root_path)
   end
-  # def destroy
-  #   @book_comment = BookComment.find(params[:id])
-  #   @book_comment.destroy
-  #   # render :index
-  # end
 
 
   private
